cd427e86888b8ecfd5962937ebd427ec5900c441
gachez <briangach98@gmail.com>
2020-02-25 13:15:39 +0300

    practical example of event emmiter in use

--- node-crash-course/index.js
+++ node-crash-course/index.js
@@ -1,5 +1,13 @@
-const Person = require('./car');
+// const Person = require('./car');
 
-let Nino = new Person('Maggie', 20)
+// let Nino = new Person('Maggie', 20)
 
-Nino.greetings()
+// Nino.greetings()
+
+const Logger = require('./logger')
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('called listener: ',data));
+
+logger.log("It works!")
